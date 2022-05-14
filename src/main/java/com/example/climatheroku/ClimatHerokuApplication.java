572b96f5f550198c918f2d2854300ee669046c28
Matthieu <95905072+MatthieuRt@users.noreply.github.com>
2022-05-14 18:04:20 +0300

    inital commit

---
+++ src/main/java/com/example/climatheroku/ClimatHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.example.climatheroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimatHerokuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClimatHerokuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/climatheroku/ClimatHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.climatheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimatHerokuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
